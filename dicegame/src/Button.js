64124375a36d5d31f3f966a24b78edc81e4c6b3a
Sangha <shhwangofficial@gmail.com>
2024-08-06 19:59:32 +0900

    Remove some code made for debugging

--- dicegame/src/Button.js
+++ dicegame/src/Button.js
@@ -1,6 +1,5 @@
 function Button({children, onClick, buttonType, color = 'blue'}) {
   let classNames = `Button ${buttonType} ${color}`;
-  console.log('classNames:', classNames); 
   return <button className={classNames} onClick={onClick}>{children}</button>;
 }
 

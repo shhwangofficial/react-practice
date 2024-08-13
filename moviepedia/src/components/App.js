788d4b89ed98dc8f981659c31182c4b128394575
Sangha <shhwangofficial@gmail.com>
2024-08-13 15:04:20 +0900

    Start moviepedia

---
+++ moviepedia/src/components/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import ReviewList from './ReviewList';
+import { getReviews } from '../api';
+
+function App() {
+  const [order, setOrder] = useState('createdAt');
+  const [items, setItems] = useState([]);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder('createdAt');
+
+  const handleBestClick = () => setOrder('rating');
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews)
+  };
+
+  useEffect(()=>
+		handleLoadClick()
+	, []);
+  
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default App;

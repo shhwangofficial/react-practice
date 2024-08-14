77f98d5a6cc0c5c1218dd98331e80909446ad1c7
Sangha <shhwangofficial@gmail.com>
2024-08-14 20:18:21 +0900

    feat: Sort and More data

--- moviepedia/src/api.js
+++ moviepedia/src/api.js
@@ -1,5 +1,6 @@
-export async function getReviews() {
-    const response = await fetch('https://learn.codeit.kr/8211/film-reviews');
+export async function getReviews({order = 'createdAt', offset = 0, limit = 6}) {
+    const query = `order=${order}&offset=${offset}&limit=${limit}`;
+    const response = await fetch(`https://learn.codeit.kr/8211/film-reviews?${query}`);
     const body = await response.json();
     return body;
 }
--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react';
 import ReviewList from './ReviewList';
 import { getReviews } from '../api';
 
+const LIMIT = 6;
+
 function App() {
   const [order, setOrder] = useState('createdAt');
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const [items, setItems] = useState([]);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -15,15 +19,26 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
-    setItems(reviews)
+
+  const handleLoad = async (options) => {
+    const { paging, reviews } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + options.limit);
+    setHasNext(paging.hasNext);
   };
 
-  useEffect(()=>
-		handleLoadClick()
-	, []);
-  
+  const handleLoadMore = async () => {
+    await handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
   return (
     <div>
       <div>
@@ -31,6 +46,9 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button disabled={!hasNext} onClick={handleLoadMore}>
+        더 보기
+      </button>
     </div>
   );
 }

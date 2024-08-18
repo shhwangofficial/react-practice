ca79e13cb9439dc3474db28109c0a90c4953adf8
shhwang0522 <shhwangofficial@gmail.com>
2024-08-18 15:00:14 +0900

    feat: Deal with network errors

--- moviepedia/src/api.js
+++ moviepedia/src/api.js
@@ -1,6 +1,10 @@
 export async function getReviews({order = 'createdAt', offset = 0, limit = 6}) {
     const query = `order=${order}&offset=${offset}&limit=${limit}`;
     const response = await fetch(`https://learn.codeit.kr/8211/film-reviews?${query}`);
-    const body = await response.json();
-    return body;
-}
+    if (!response.ok) {
+        throw new Error('리뷰를 불러오는데 실패했습니다');
+      }
+      const body = await response.json();
+      return body;
+    }
+    
--- moviepedia/src/components/App.js
+++ moviepedia/src/components/App.js
@@ -8,9 +8,9 @@ function App() {
   const [order, setOrder] = useState('createdAt');
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
-  const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [loadingError, setLoadingError] = useState(null);
+  const [items, setItems] = useState([]);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
@@ -25,19 +25,21 @@ function App() {
   const handleLoad = async (options) => {
     let result;
     try {
-      setIsLoading(true)
+      setLoadingError(null);
+      setIsLoading(true);
       result = await getReviews(options);
-    } catch(error) {
-      console.error(error);
+    } catch (error) {
+      setLoadingError(error);
       return;
     } finally {
       setIsLoading(false);
     }
-	  const { paging, reviews } = result;
+
+    const { paging, reviews } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
-      setItems((currentItems) => [...currentItems, ...reviews]);
+      setItems((prevItems) => [...prevItems, ...reviews]);
     }
     setOffset(options.offset + options.limit);
     setHasNext(paging.hasNext);
@@ -58,9 +60,12 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      { hasNext && <button disabled={isLoading} onClick={handleLoadMore}>
-        더 보기
-      </button> }
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 }

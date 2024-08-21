d223b36cabea9639e64d127a68e2807cc415ca6a
Sangha <shhwangofficial@gmail.com>
2024-08-21 19:26:10 +0900

    Start Pokemon type table project

---
+++ pokemontypetable/src/App.js
@@ -0,0 +1,12 @@
+import SmallTable from './SmallTable.js';
+import BigTable from './BigTable.js';
+function App() {
+  return (
+    <div>
+      <SmallTable />
+      <BigTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemontypetable/src/SmallTable.js
@@ -0,0 +1,5 @@
+function SmallTable() {
+
+}
+
+export default SmallTable;
